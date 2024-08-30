2e8c34b6492b5a6267cddeb31789d01c567b2405
MatiasPerez00 <matiasperez.personal@gmail.com>
2024-08-30 11:54:24 -0300

    Dependencias cargadas:hookform,reactBootstrap. Creado label e input correo

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './Login.css'
+import { Container, Form, FormControl, FormGroup, FormLabel} from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
+  const {register,handleSubmit,formState:{errors}}=useForm({mode:'onBlur'});
   return (
-    <div>Login</div>
+    <div className='login'>
+      <Container fluid className='container-form'>
+        <h2>L O G I N</h2>
+        <Form>
+          <FormGroup>
+            <FormLabel>Ingresa tu correo:</FormLabel>
+            <FormControl
+              type='email'
+              name='email'
+              {...register('email',{
+                  required:'El email es requerido',
+                  minLength:{
+                    value:12,
+                    message:'El correo debe tener mas de 10 caracteres en total'
+
+                  },
+                  maxLength:{
+                    value:40,
+                    message:'El correo no debe tener mas de 40 caracteres en total'
+                  }
+
+                }) }
+            />
+          </FormGroup>
+        </Form>
+      </Container>
+    </div>
+    
+    
   )
 }
 
