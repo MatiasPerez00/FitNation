5add9bc12c5d9cdaed0cd7fbd7bf3cf013f717e1
MatíasPérez00 <matiasperez.personal@gmail.com>
2024-09-03 00:17:30 -0300

    agragado campo contraseña

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Login.css'
-import { Container, Form, FormControl, FormGroup, FormLabel} from 'react-bootstrap'
+import { Container, Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 
 const Login = () => {
-  const {register,handleSubmit,formState:{errors}}=useForm({mode:'onBlur'});
+  const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onBlur' });
   return (
     <div className='login'>
       <Container fluid className='container-form'>
@@ -15,26 +15,46 @@ const Login = () => {
             <FormControl
               type='email'
               name='email'
-              {...register('email',{
-                  required:'El email es requerido',
-                  minLength:{
-                    value:12,
-                    message:'El correo debe tener mas de 10 caracteres en total'
+              {...register('email', {
+                required: 'El email es requerido',
+                minLength: {
+                  value: 12,
+                  message: 'El correo debe tener mas de 10 caracteres en total'
 
-                  },
-                  maxLength:{
-                    value:40,
-                    message:'El correo no debe tener mas de 40 caracteres en total'
-                  }
+                },
+                maxLength: {
+                  value: 40,
+                  message: 'El correo no debe tener mas de 40 caracteres en total'
+                }
 
-                }) }
+              })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Ingresa tu contraseña:</FormLabel>
+            <FormControl
+              type='password'
+              name='password'
+              {...register('password', {
+                required: 'La contraseña es requerido',
+                minLength: {
+                  value: 6,
+                  message: 'La contraseñao debe tener al menos 6 caracteres'
+
+                },
+                maxLength: {
+                  value: 20,
+                  message: 'La contraseña no debe tener mas de 20 caracteres en total'
+                }
+
+              })}
             />
           </FormGroup>
         </Form>
       </Container>
     </div>
-    
-    
+
+
   )
 }
 
